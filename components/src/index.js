2ea7caf3a5c2b9ea4acd97f98dde0b9d441d96ca
Abdul Mohsin <abdul.mohsin@burgessgroup.com>
2019-10-17 16:25:24 -0400

    Rearrange the source directory

--- components/src/index.js
+++ components/src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
 
-ReactDOM.render(<App />, document.getElementById('root'));
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+          <div className="metadata">
+            <span className="date">Today at 6:00PM</span>
+          </div>
+          <div className="text">Nice Blog post !!!</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
