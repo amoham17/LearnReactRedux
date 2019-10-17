f53ae26a355904056738cff2625dec0b5528703e
Abdul Mohsin <abdul.mohsin@burgessgroup.com>
2019-10-17 14:24:39 -0400

    Some commit

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,6 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div>Hi there !</div>;
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter Name:
+      </label>
+      <input id="name" type="text" />
+
+      <button style={{ backgoundColor: "blue", color: "white" }}>Submit</button>
+    </div>
+  );
 };
 ReactDOM.render(<App />, document.querySelector("#root"));
